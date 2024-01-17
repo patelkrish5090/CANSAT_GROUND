25be262a8797874077149394b1176a59e19f8c6c
agnibha11 <67596749+agnibha11@users.noreply.github.com>
2024-01-17 22:36:39 +0530

    ver 1

---
+++ Telemetry/fsa.ino
@@ -0,0 +1,31 @@
+//declare state variables here
+int state;
+int altitude;
+
+void setup()
+{
+  //initilize state variables here
+  state = 0; //initial state
+}
+
+void loop()
+{
+  
+}
+
+void simulation()
+{
+    //write simulation code here
+    
+}
+
+void FSA()
+{
+    //write flight system algorithm here
+}
+
+//TODO: add argument for the function
+void writeSD() //write the data as argument
+{
+
+}
